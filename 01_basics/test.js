1d54a4bd737474ef752a8cb672280db54fa4fb50
dheerajoy <116648831+dheerajoy@users.noreply.github.com>
2024-04-24 05:43:22 +0000

    variables intro

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const userId = 23; // remain constant connot be changed once declared
+let userName = "Dheeraj"; // can be changed later on
+var userPassword = "1234";  // prefer not to use because of scope issue
+userLocation = "bangalore"; // can declare a variable even without keyword
+let userAge; // if a variable is declared and a value is not assigned it shows undefined on log , can assign value later on 
+
+console.log(userId);
+
+//userId="123"; // will not compile because of const keyword
+
+console.table([userId,userName,userPassword,userLocation,userAge]);
