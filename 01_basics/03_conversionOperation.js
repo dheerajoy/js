2fb3e2245144c2e179d3a494c87208d8788faaea
dheerajoy <116648831+dheerajoy@users.noreply.github.com>
2024-06-24 17:06:14 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "10abc";
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let scoreAsNumber = Number(score);
+
+//console.log(typeof scoreAsNumber);
+//console.log(scoreAsNumber);
+
+let number = false;
+//console.log(number);
+//console.log(typeof (number));
+
+let numberConversion = Number(number);
+//console.log(typeof numberConversion);
+//console.log(numberConversion);
+
+
+// number convert easily 
+// "33" => 33
+// '33abc' => Nan
+// true => 1; false => 0
+
+let loggedIn = "hh";
+
+let booleanLoggedIn = Boolean(loggedIn);
+console.log(booleanLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "dhee" => true;
+
+let count = 10;
+
+let countToString = String(count);
+console.log(countToString);
+console.log(typeof countToString);
