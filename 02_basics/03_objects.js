d72a7898007bae56d6c617429980d6ba5184c9f8
dheerajoy <dheerajsuthar46@gmail.com>
2024-09-14 23:18:43 +0530

    learnt about objects basis

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,18 +3,44 @@
 
 // objects literals { }
 
-
+const mySym = Symbol('key')
 const user = {
     name: 'dheeraj',
     "last name": 'kumar',
+    email: 'dheeraj@gmail.com',
     age: 16,
     city: [ "bangalore" , "jaipur"],
     loggedIn: false,
+    [mySym]: 'key', // using symbol in object
 }
 
 // To access the value
-console.log(user.name);
-console.log(user["last name"]);
+// console.log(user.name); // dot notation
+// console.log(user["last name"]); // bracket notation
+
+// console.log(user[mySym]); // to access symbol in objects
+
+
+user.email = 'new-email.com'
+// Object.freeze(user)  // will not allow to change object
+user.email = 'change-again-email.com'
+
+// console.log(user);
+
+user.greeting = function (){
+    console.log(`hello user`);
+}
+
+user.greetingTwo = function (){
+    console.log(`hello user, ${this.name}`); // this keyword in object to access values of current object
+}
+
+console.log(user.greeting); // [Function (anonymous)]
+console.log(user.greetingTwo); // [Function (anonymous)]
+
+console.log(user.greeting()); // hello user undefined
+console.log(user.greetingTwo()); // hello user, dheeraj undefined
+
 
 
 
