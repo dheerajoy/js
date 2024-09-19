0140ad031ba7db7fba40cd115dd55915b92dd7f6
dheerajoy <dheerajsuthar46@gmail.com>
2024-09-19 11:20:25 +0530

    iife changes

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,14 +1,16 @@
 // Immediately Invoked Function Expression (IIFE)
 
-function DBConnection() {
-  console.log("DB Connected");
-}
-
-(function DBConnection() {
-  // named iife
-  console.log("DB Connected");
+(function () {
+  console.log("I am an IIFE");
 })(); // dont miss semi colon
 
+// named iife
+(function DBConnection() {
+  console.log("DB Connected");
+})();
+
+// iife as arrow function with parameters
 ((name) => {
   console.log(`${name}, you are connected to DB!`);
+  console.log(this); // {}
 })("dheeraj");
