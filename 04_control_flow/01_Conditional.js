57022437d9fbd329c24a5aa1db329f1ecab2bee9
dheerajoy <dheerajsuthar46@gmail.com>
2024-09-19 22:55:48 +0530

    conditonals

---
+++ 04_control_flow/01_Conditional.js
@@ -0,0 +1,21 @@
+// Conditional Statements
+
+// if (true) {
+//   console.log("Hello");
+// }
+
+// const age = 21;
+
+// if (age > 21) {
+//   console.log("Under age limit to Vote!");
+// } else {
+//   console.log("Eligeble to Vote!");
+// }
+
+// Conditional Operators:  > , < , <= , >= , == , != , ===
+
+let x = "3";
+
+if (x !== 3) {
+  console.log("I am logged");
+}
