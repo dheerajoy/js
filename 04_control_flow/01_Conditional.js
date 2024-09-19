2bfe26a5f28440bd80134a74a4722a10848b9e85
dheerajoy <dheerajsuthar46@gmail.com>
2024-09-19 23:25:39 +0530

    learnt if else , conditional operators and switch

--- 04_control_flow/01_Conditional.js
+++ 04_control_flow/01_Conditional.js
@@ -14,8 +14,36 @@
 
 // Conditional Operators:  > , < , <= , >= , == , != , ===
 
-let x = "3";
+// let x = "3";
 
-if (x !== 3) {
-  console.log("I am logged");
-}
+// if (x !== 3) {
+//   console.log("I am logged");
+// }
+
+// if - else
+
+// const num = 0;
+
+// if (num < 0) {
+//   console.log("- Negative Integer");
+// } else if (num <= 0) {
+//   console.log("Its Zero");
+// } else {
+//   console.log("+ Positive Integer");
+// }
+
+// && AND    both the side needs to be true
+// const userLoggedIn = true;
+// const debitCard = true;
+
+// // if (userLoggedIn && debitCard) {
+// //   console.log("allow user to buy");
+// // }
+
+// || OR    any one of the side needs to be true
+// const googleMail = true;
+// const microSoftMail = false;
+
+// if (googleMail || microSoftMail) {
+//   console.log("valid user");
+// }
---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,28 @@
+// Conditional Switch
+
+//syntax
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+
+const day = 1;
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("webnesday");
+    break;
+
+  default:
+    console.log("Unknown day");
+    break;
+}
