6e96fd078f541b1d97b91dfdde57039ddbb79489
Dheeraj <116648831+dheerajoy@users.noreply.github.com>
2024-09-20 17:41:51 +0000

    for , while , do while loop

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,59 @@
+// while
+
+// let i = 0
+
+// while (i <= 10) {
+//     console.log(`i = ${i}`);
+//     i = i + 2;
+// }
+
+// i = 0
+// i = 2
+// i = 4
+// i = 6
+// i = 8
+// i = 10
+
+// let a = 0;
+// while (a <= 10) {
+//   console.log(a);
+  
+//   if ((a == 5)) {
+//     console.log("its 5");
+//     break;
+//   }
+//   a = a + 1;
+// }
+
+
+// const fruits = ['banana' , 'apple' , 'kiwi'];
+
+// let arr = 0
+// while(arr < fruits.length){
+//     console.log(fruits[arr])
+//     arr = arr + 1
+// }
+
+
+// do while
+
+// let num = 1;
+// do {
+//     console.log(`num is ${num}`);
+//     num++
+// } while (num <= 5);
+
+// num is 1
+// num is 2
+// num is 3
+// num is 4
+// num is 5
+
+
+// let num = 5;
+// do {
+//     console.log(`num is ${num}`);
+//     num++
+// } while (num <= 5);
+
+// num is 5
