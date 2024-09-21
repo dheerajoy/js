b917b0d7a8471ec408448d28d5b6a0a8425f92ce
Dheeraj <116648831+dheerajoy@users.noreply.github.com>
2024-09-21 17:28:07 +0000

    for of loop

---
+++ 05_iterations/for_of.js
@@ -0,0 +1,15 @@
+// ['','','']
+// [{},{},{}]
+// for of
+
+// const arrMonth = ['jan','feb','mar','apr']
+
+// for (const element of arrMonth) {
+//     console.log(element);
+// }
+
+const greet = "hello, world";
+
+for (let i of greet) {
+  console.log(`character: ${i}`);
+}
