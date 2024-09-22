ff7e3a256d842b709867e16e0ff5f95820725812
Dheeraj <116648831+dheerajoy@users.noreply.github.com>
2024-09-22 18:05:31 +0000

    reduce on object

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,12 +1,38 @@
 // reduce
 
-const arr = [1,2,3]
+const arr = [1, 2, 3];
 
 // const total = arr.reduce(function (acc , cur){
 //     console.log(`acc:${acc} cur:${cur}`);
 //     return acc + cur
 // },0)
 
-const total = arr.reduce((acc, cur)=> acc + cur,0)
+const total = arr.reduce((acc, cur) => acc + cur, 0);
 
-console.log(total);
+// console.log(total);
+
+const cart = [
+  {
+    item: "bag",
+    price: 200,
+  },
+  {
+    item: "shoe",
+    price: 1200,
+  },
+  {
+    item: "pen",
+    price: 10,
+  },
+  {
+    item: "books",
+    price: 550,
+  },
+];
+
+
+const cartTotal = cart.reduce((acc , item) => {
+    return acc + item.price
+},0)
+
+console.log(cartTotal);
