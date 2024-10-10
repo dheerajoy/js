e2682564e5490b8eba5139ce4923d37423659832
dheerajoy <dheerajsuthar46@gmail.com>
2024-10-10 23:24:29 +0530

    prototype new

--- 10_classes_&_oops/objects.js
+++ 10_classes_&_oops/objects.js
@@ -7,3 +7,21 @@ add2.power = 2;
 console.log(add2(2));
 console.log(add2.power);
 console.log(add2.prototype);
+
+function createUser(userName, age) {
+  this.userName = userName;
+  this.age = age;
+}
+
+createUser.prototype.increment = function () {
+  this.age++;
+};
+
+createUser.prototype.printMe = function () {
+  return console.log(`age is ${this.age}`);
+};
+
+const dheeraj = new createUser("dheeraj", 23);
+const kumar = new createUser("kumar", 10);
+
+dheeraj.printMe();
